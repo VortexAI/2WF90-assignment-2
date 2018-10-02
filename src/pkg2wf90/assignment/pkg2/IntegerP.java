1e0f750011e8078b43735d0fc43b34f004125113
VortexAI <s.c.a.beukers@student.tue.nl>
2018-10-02 16:45:35 +0200

    look at me plebs, did all the work lmfao

--- src/pkg2wf90/assignment/pkg2/IntegerP.java
+++ src/pkg2wf90/assignment/pkg2/IntegerP.java
@@ -31,35 +31,23 @@ public class IntegerP {
      * adds val2 to this IntegerP
      * @param val2 IntegerP to be added
      */
-    void add(IntegerP val2){
-        if((this.val + val2.getVal())<0){
-            this.val = (this.val + val2.getVal())%mod + mod;
-        } else {
-            this.val = (this.val + val2.getVal())%mod;
-        }
+    IntegerP add(IntegerP val2){
+        return new IntegerP(this.val + val2.getVal(), mod);
     }
     
     /**
      * subtracts val 2 from this IntegerP
      * @param val2 number to be subtracted by
      */
-    void subtract(IntegerP val2){
-        if((this.val - val2.getVal())<0){
-            this.val = (this.val - val2.getVal())%mod + mod;
-        } else {
-            this.val = (this.val - val2.getVal())%mod;
-        }
+    IntegerP subtract(IntegerP val2){
+        return new IntegerP(this.val - val2.getVal(), mod);
     }
     
     /**
      * multiplies this InteregerP by val2
      * @param val2 the number to be mulitplied with
      */
-    void mult(IntegerP val2){
-        if((this.val * val2.getVal())<0){
-            this.val = (this.val * val2.getVal())%mod + mod;
-        } else {
-            this.val = (this.val * val2.getVal())%mod;
-        }
+    IntegerP mult(IntegerP val2){
+        return new IntegerP(this.val * val2.getVal(), mod);
     }
 }
--- src/pkg2wf90/assignment/pkg2/Poly.java
+++ src/pkg2wf90/assignment/pkg2/Poly.java
@@ -10,5 +10,96 @@ package pkg2wf90.assignment.pkg2;
  * @author s151810
  */
 public class Poly {
+    private IntegerP[] poly;
+    private int mod;
     
+    Poly(int[] a, int mod){
+        this.mod = mod;
+        poly = new IntegerP[a.length];
+        for(int i = 0; i < a.length; i++){
+            poly[(a.length-1)-i] = new IntegerP(a[i], mod);
+        }
+    }
+    
+    Poly(IntegerP[] poly, int mod){
+        this.poly = poly;
+        this.mod = mod;
+    }
+    
+    public IntegerP[] getPoly(){
+        return poly;
+    }
+    
+    Poly add(Poly g){
+        IntegerP[] temp;
+        
+        if(g.getPoly().length > this.poly.length){
+            temp = new IntegerP[g.getPoly().length];
+            for(int i = poly.length; i < g.getPoly().length; i++){
+                temp[i] = g.getPoly()[i];
+            }
+        } else {
+            temp = new IntegerP[this.poly.length];
+            for(int i = g.getPoly().length; i < poly.length; i++){
+                temp[i] = poly[i];
+            }
+        }
+        for(int i = 0; (i < Math.min(g.getPoly().length, poly.length)); i++){
+            temp[i] = poly[i].add(g.getPoly()[i]);
+        }
+        
+        return new Poly(temp, mod);
+    }
+    
+    Poly subtract(Poly g){
+        IntegerP[] temp;
+        
+        if(g.getPoly().length > this.poly.length){
+            temp = new IntegerP[g.getPoly().length];
+            for(int i = poly.length; i < g.getPoly().length; i++){
+                temp[i] = new IntegerP(-g.getPoly()[i].getVal(), mod);
+            }
+        } else {
+            temp = new IntegerP[this.poly.length];
+            for(int i = g.getPoly().length; i < poly.length; i++){
+                temp[i] = poly[i];
+            }
+        }
+        for(int i = 0; (i < Math.min(g.getPoly().length, poly.length)); i++){
+            temp[i] = poly[i].subtract(g.getPoly()[i]);
+        }
+        
+        return new Poly(temp, mod);
+    }
+    
+    Poly multiply(Poly g){
+        IntegerP[] temp = new IntegerP[1+g.degree()+this.degree()];
+        
+        for(IntegerP a : temp){
+            a = new IntegerP(0, mod);
+        }
+        
+        for(int i = 0; i<=this.degree(); i++){
+            for(int j = 0; j <= g.degree(); j++){
+                temp[i+j] = temp[i+j].add(g.getPoly()[j].mult(poly[j]));
+            }
+        }
+        return new Poly(temp, mod);
+    }
+    
+    //TODO
+    IntegerP leadingCoefficient(){
+        return poly[this.degree()];
+    }
+    
+    int degree(){
+        int result = 0;
+        for(int i = poly.length-1; i<0; i--){
+            if(poly[i].getVal() != 0){
+                result = i;
+                break;
+            }
+        }
+        return result;
+    }
 }
