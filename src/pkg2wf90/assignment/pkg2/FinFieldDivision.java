7e7366c5ecbc926d630613bfb32669b892cdbb1e
TtomtomT <velden.tom@gmail.com>
2018-10-19 22:23:54 +0200

    added comments

--- src/pkg2wf90/assignment/pkg2/FinFieldDivision.java
+++ src/pkg2wf90/assignment/pkg2/FinFieldDivision.java
@@ -5,13 +5,17 @@
  */
 package pkg2wf90.assignment.pkg2;
 
-/**
- *
- * @author s166268
- */
+
 public class FinFieldDivision {
    
+    /** Returns the result of the division of 2 given polynomials of a field
+    * 
+    * @param f The first polynomial
+    * @param g The second polynomial
+    * @param field The given finite field
+    */
     public Poly run(Poly f, Poly g, FinField field) {
+        // compute division f/g by multiplying f by the inverse of g
         Inverse inverse = new Inverse();
         Poly gInverse = inverse.run(field, g);
         return field.multiply(f, gInverse);
