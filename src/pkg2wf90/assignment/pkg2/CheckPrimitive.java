db9ed9e9d6e5bf631cfc129252d3425af15396a4
StijnTruijen <43032557+StijnTruijen@users.noreply.github.com>
2018-10-27 17:29:48 +0200

    CheckPrimitivefix?

--- src/pkg2wf90/assignment/pkg2/CheckPrimitive.java
+++ src/pkg2wf90/assignment/pkg2/CheckPrimitive.java
@@ -13,31 +13,29 @@ import java.util.List;
  * @author stijn
  */
 public class CheckPrimitive {
-    Poly fieldPoly;
-    
+
     public boolean run(FinField f, Poly input){
-        fieldPoly = new Poly(f.getPoly(),7);
+        //Get the order of the field
         int q = f.getElements().length;
-       
+        
+        //Get the poly of the input to check for 0 polys
         IntegerP[] poly = input.getPoly();
 
         boolean zero = true;
 
+        //Check if the poly is all zeros
         for(int i=0; i < poly.length; i++){
             if(poly[i].getVal() != 0){
                 zero = false;
                 break;
             }
         }
-
-        if(zero){           
+        //If all zeros return false
+        if(zero){
             return false;
         }
 
-
-        Poly a = input;
-        Poly one = new Poly(new int[]{1}, f.getMod());
-
+        //Find all prime divisors of the Order - 1
         List<Integer> primeDivisors = new ArrayList<>();
         boolean isPrime = true;
         for(int i=2; i<q; i++){
@@ -50,15 +48,20 @@ public class CheckPrimitive {
                     }
                 }
                 if(isPrime){
-                   
                     primeDivisors.add(i);
                 }
             }
         }
-
+        
+        
+        //The input poly and the Poly "One" for condition checking
+        Poly a = input;
+        Poly one = new Poly(new int[]{1}, f.getMod());
+        //Main algorithm
+        //Put a to the power of each our prime divisors and check if it is euqal to one.
         int i = 1;
         while(i <= primeDivisors.size()) {
-            if (toThePower(a, (q - 1) / primeDivisors.get(i - 1), f.getMod()).equals(one)) {
+            if (toThePower(a, (q - 1) / primeDivisors.get(i - 1), f.getMod(), f, one).equals(one)) {
                 break;
             } else {
                 i += 1;
@@ -70,19 +73,31 @@ public class CheckPrimitive {
         } else{
             return true;
         }
-    }
 
-    public Poly toThePower(Poly a, int exponent, int mod){
-       
+
+    }
+    
+    //Function used for exponantion. Multiplies a poly by itself for the size of the
+    //exponent. If an intermediate power is found twice, a cycle is found to early and
+    //we return a One poly so the main algorithm will evaluate to false. 
+    
+    public Poly toThePower(Poly a, int exponent, int mod, FinField f, Poly one){
+        List<String> powers = new ArrayList<>();
         Poly power = new Poly(a.getPoly(), mod);
-        LongDivision division = new LongDivision();
 
 
         for(int i=0; i<exponent-1; i++){
-            power = division.run(power.multiply(a), fieldPoly)[1];
-        } 
-        //Poly powerMod = division.run(power, fieldPoly)[1];
-       
+            power = f.multiply(power, a);
+            if(powers.contains(power.display())){
+                power = one;
+                break;
+            } else{
+                powers.add(power.display());
+            }
+
+        }
+
         return power;
     }
 }
+
